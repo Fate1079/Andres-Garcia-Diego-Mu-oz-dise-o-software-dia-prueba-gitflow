a754f2091c060765729ba2b9d9273d3097fe8efb
Fate <andresfeli107911@gmail.com>
2025-02-28 08:45:23 -0500

    Programador A agregado commit inicial

---
+++ src/main/java/com/mycompany/pruebagitflow2/PruebaGitflow2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagitflow2;
+
+/**
+ *
+ * @author ANDRES FELIPE
+ */
+public class PruebaGitflow2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
