580b1bf923ef1c5bb4b8b30acc2880b3e53439cc
DiegoMB3254 <diegoamunoz2005@gmail.com>
2025-02-28 08:59:31 -0500

    ProgramadorB a agregado metodoA

---
+++ src/main/java/com/mycompany/pruebagitflow2/Inventario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebagitflow2;
+
+/**
+ *
+ * @author MI PC
+ */
+public class Inventario {
+//metodos
+    public void metodoA() {
+        System.out.println("Ejecutando método A");
+    }
+
+}
